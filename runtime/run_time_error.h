91e15732e18ffa29e9aa61fe06bf637dfc85295b
Logan <12logan.welty@gmail.com>
2024-10-18 22:45:01 -0700

    Added Doc Strings the run_time_error.h

--- runtime/run_time_error.h
+++ runtime/run_time_error.h
@@ -11,31 +11,54 @@ namespace RunTimeError {
                 message_ = message;
                 setType(type);
             };
-
             // Default constructor
             explicit runtimeerror() { message_ = "Unspecified runtime error"; };
-
+            /** -----------------------------------------------------------------
+             * @brief Simple getter method
+             *
+             * @returns a string literal which is associated with the generic type 'Sig'
+             * ------------------------------------------------------------------
+            */
             inline static const char* getMsg() { return message_; };
-
             // Default destructor
             ~runtimeerror() = default;
-
-            // Get the error message
+            /** -----------------------------------
+             * @brief A static polymorphism method that will reseat the object by calling the correct class instance method
+             * 
+             * @param Sig Is a concrete type that is checked during compilation; preferable TokenType
+             * @param mesg the custom message that will be logged 
+             * 
+             * @details Requires the template parameter syntax <> in order to use it 
+             * 
+             * @return Returns the derived class instance, 'this' method
+            */
             template<typename Sig>
-            inline const char* what(const Sig& type, const char* msg) {
-                return static_cast<Type*>(this)->what(type, msg);
-            };
-        
+            inline const char* what(const Sig& type, const char* msg) { return static_cast<Type*>(this)->what(type, msg); };
+            /** -----------------------------------
+             * @brief Get the token associated with the error
+             * 
+             * @details Requires the template parameter syntax <> in order to use it 
+             * 
+             * @return Returns the derived class instance, 'this' method
+            */
+            template<typename Sig>
+            inline const Sig& getType() { return static_cast<Type*>(this)->getType(); };
+        protected:
+            inline static void* type = nullptr;
+        private:
+            inline static const char* message_{};
+            /** -----------------------------------
+             * @brief A static polymorphism method that will reseat the object by calling the correct class instance method
+             * 
+             * @param Sig Is a concrete type that is checked during compilation; preferable TokenType
+             * 
+             * @details Requires the template parameter syntax <> in order to use it 
+             * 
+             * @return Returns nothing
+            */
             template<typename Sig>
             inline void setType(const Sig& value) { type = const_cast<void*>(static_cast<const void*>(&value)); };
 
-            // Get the token associated with the error
-            template<typename Sig>
-            inline const Sig& getType() { return static_cast<Type*>(this)->getType(); };
-
-        protected:
-            inline static void* type = nullptr;
-            inline static const char* message_{};
     };
 };
 using namespace RunTimeError;
